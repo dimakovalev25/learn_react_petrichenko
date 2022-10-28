8a55ee56fc3ecb36a938ae1f520423f068b44d49
dimakovalev25 <d.nozdryn@gmail.com>
2022-10-28 21:20:22 +0300

    bind in function

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import React from 'react';
 import './App.css';
 import Car from "./components/Car";
 
-function App() {
-    return (
-        <div>
-            <h1>
-                HELLO REACT! <br/>
-                <Car name={'audi'} year={2018}>
-                    <p>c</p>
-                </Car>
-                <Car name={'bugatti'} year={2018}/>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'audi', year: 2018},
+                {name: 'bugatti', year: 2018},
+            ],
+            pageTitle: 'HELLO REACT!'
+        }
+    }
 
-            </h1>
-        </div>
-    )
+    changeTitle = (newTitle) => {
+        this.setState({
+            pageTitle: newTitle
+        })
+    }
 
+
+    render() {
+    const cars = this.state.data;
+        return (
+            <div>
+                <h1>
+                    {this.state.pageTitle} <br/>
+                    <button
+                        onClick={this.changeTitle.bind(this, "Changed!")}
+                    >Change</button>
+                    <Car
+                        name={'audi'}
+                        year={2018}
+                        onchangeTitle={this.changeTitle.bind(this, 'Car')}
+                    />
+                    <Car name={'bugatti'} year={2018}/>
+
+                </h1>
+            </div>
+        )
+
+    }
 }
 
 export default App;
--- src/components/Car.js
+++ src/components/Car.js
@@ -3,8 +3,11 @@ function Car(props) {
     return (
         <div>
             car name: {props.name} <br/>
-            year:{props.year}
-            {props.children}
+            year: {props.year} <br/>
+            <button
+                onClick={props.onchangeTitle}
+            >Change title!</button>
+
         </div>
     )
 
